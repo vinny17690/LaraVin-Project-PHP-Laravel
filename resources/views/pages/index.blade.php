94f0bc3ad3e261693be2bec0e33411d77e7a28fd
vinny17690 <vinay.vinny18@gmail.com>
2017-06-19 22:48:26 -0400

    Full project commit Version 1.0

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     <div class="jumbotron text-center">
-        <h1>Welcome To Laravel!</h1>
-        <p>This is the laravel application from the "Laravel From Scratch" YouTube series</p>
+        <h1>Welcome To LaraVin App</h1>
+        <p>This is a Blog Application designed using Laravel</p>
         <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
     </div>
 @endsection
